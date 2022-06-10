37d4e54bf88b0acf96347d5398bb23d278a8fe11
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-06-10 20:21:23 -0300

    commit get all movies

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -28,3 +28,13 @@ export async function findMovieById(req: Request, res: Response) {
         Logger.error(`Erro no sistema: ${error.message}`)
     }
 }
+
+export async function getAllMovies(req: Request, res: Response) {
+    try {
+        const movies = await MovieModel.find()
+        return res.status(200).json(movies)
+    } catch (error: any) {
+        Logger.error(`Erro no sistema: ${error.message}`)
+
+    }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express"
-import { createMovie, findMovieById } from "./controllers/movieControllers"
+import { createMovie, findMovieById, getAllMovies } from "./controllers/movieControllers"
 
 // Validations
 import { validate } from "./middleware/handleValidation"
@@ -12,3 +12,4 @@ export default router.get("/test", (req: Request, res: Response) => {
 })
 .post("/movie", movieCreateValidation(), validate, createMovie)
 .get("/movie/:id", findMovieById)
+.get("/movie", getAllMovies)
